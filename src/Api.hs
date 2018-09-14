f231eb5b631ac9943371d8ef95ec4e80858d3f2d
Johannes Groedem <johs@grdm.no>
2018-09-14 23:30:29 +0200

    Add debug API, cleanups.

--- src/Api.hs
+++ src/Api.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE DataKinds     #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators     #-}
 
 module Api (app) where
 
@@ -13,29 +14,33 @@ import           Servant
 import           Storage
 import           Types
 
-type API = "sensor" :> Header "Authorization" T.Text :>
+type SensorAPI = "sensor" :> Header "Authorization" T.Text :>
            (Get '[JSON] [Sensor]
             :<|> ReqBody '[JSON] Reading :> Post '[JSON] Reading
             :<|> Capture "name" SensorName :> Get '[JSON] [Reading])
 
-server :: Session -> Server API
-server session@(Session _ secret) auth =
+type DebugAPI = "debug" :> Header "Debug" T.Text :> Get '[JSON] T.Text
+
+type API = SensorAPI :<|> DebugAPI
+
+sensorServer :: Session -> Server SensorAPI
+sensorServer session@(Session _ secret) auth =
   sensors auth :<|> newReading auth :<|> readings auth
 
-  where authMe :: Handler ()
-        authMe =
+  where checkAuthorization :: Handler ()
+        checkAuthorization =
           case auth of
             Just s | s == secret -> return ()
             _                    -> throwError err401
 
         sensors :: Maybe T.Text -> Handler [Sensor]
         sensors _ = do
-          authMe
+          checkAuthorization
           liftIO $ getSensors session
 
         newReading :: Maybe T.Text -> Reading -> Handler Reading
         newReading _ reading = do
-          authMe
+          checkAuthorization
           r <- liftIO $ insertReading session reading
           case r of
             Left s         -> throwError err400 { errReasonPhrase = convertString s }
@@ -43,11 +48,24 @@ server session@(Session _ secret) auth =
 
         readings :: Maybe T.Text -> SensorName -> Handler [Reading]
         readings _ name = do
-          authMe
+          checkAuthorization
           liftIO $ getReadings session name
 
-sensorAPI :: Proxy API
-sensorAPI = Proxy
+debugServer :: Server DebugAPI
+debugServer = debug
+
+  where
+    debug :: Maybe T.Text -> Handler T.Text
+    debug t =
+      case t of
+        Just t' -> return $ "Heisann, " `T.append` t' `T.append` "!"
+        Nothing -> return $ "Hoi!"
+
+apiServer :: Session -> Server API
+apiServer session = sensorServer session :<|> debugServer
+
+serverAPI :: Proxy API
+serverAPI = Proxy
 
 app :: Session -> Application
-app session = serve sensorAPI $ server session
+app session = serve serverAPI $ apiServer session
