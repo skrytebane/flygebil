cd10f649dc4928062b6c05fa214360d8e18bce21
Johannes Groedem <johs@grdm.no>
2018-09-15 08:58:29 +0200

    Read secret from a file or stdin.

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,13 @@
 
 module Main where
 
+import           Data.String.Conversions  (convertString)
+import qualified Data.Text.Lazy           as T
 import           Database.SQLite.Simple
 import           Network.Wai.Handler.Warp
+import           System.Environment       (getArgs)
+import           System.IO                (IOMode (ReadMode), hGetContents,
+                                           withFile)
 
 import           Api                      (app)
 import           Storage                  (initializeTables)
@@ -13,6 +18,17 @@ main :: IO ()
 main =
   withConnection "readings.db"
   (\conn -> do
-      let session = Session conn "supersekrit"
+      secret <- getSecret "supersekrit"
+      let session = Session conn secret
       initializeTables session
       run 8081 $ app session)
+
+  where getSecret :: String -> IO T.Text
+        getSecret default' = do
+          args <- getArgs
+          let contents =
+                case args of
+                  "-" : _      -> getContents
+                  fileName : _ -> withFile fileName ReadMode hGetContents
+                  _            -> return default'
+          T.strip . convertString <$> contents
